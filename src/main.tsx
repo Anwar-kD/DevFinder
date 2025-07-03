cee9dcfcf3cb769ebedcb62f38002721e9a3cf54
anwar-kd <202311789@cegepsherbrooke.qc.ca>
2025-07-03 19:46:06 +0000

    Ajout du téléchargement de CV optimisé en HTML

--- src/main.tsx
+++ src/main.tsx
@@ -1,52 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
-import Welcome from './components/Welcome';
-import { SignUp } from './components/SignUp';
-import { Login } from './components/Login';
-import { AuthProvider } from './context/AuthContext'; 
-import { Dashboard } from './components/Dashboard';
-import PrivateRoute  from './components/PrivateRoute';
-import { Offers } from './components/Offers';
-import { JobApplication } from './components/Application';
-import { CVOptimizer } from './components/CVOptimizer';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Welcome from "./components/Welcome";
+import { SignUp } from "./components/SignUp";
+import { Login } from "./components/Login";
+import { AuthProvider } from "./context/AuthContext";
+import { Dashboard } from "./components/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
+import { Offers } from "./components/Offers";
+import { JobApplication } from "./components/Application";
+import CVOptimizer from "./components/CVOptimizer";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Welcome />,
   },
   {
-    path: '/offers',
+    path: "/offers",
     element: <Offers />,
   },
   {
-    path: '/sign-up',
-    element: <SignUp />
+    path: "/sign-up",
+    element: <SignUp />,
   },
   {
-    path: '/login',
-    element: <Login />
+    path: "/login",
+    element: <Login />,
   },
   {
-    path: '/dashboard',
-    element: (<PrivateRoute><Dashboard /></PrivateRoute>), 
+    path: "/dashboard",
+    element: (
+      <PrivateRoute>
+        <Dashboard />
+      </PrivateRoute>
+    ),
   },
   {
-    path: '/application/:offerId',
-    element: <JobApplication />, 
+    path: "/application/:offerId",
+    element: <JobApplication />,
   },
-    {
-    path: '/cvoptimizer',
-    element: <CVOptimizer />, 
-  }
+  {
+    path: "/cvoptimizer",
+    element: <CVOptimizer />,
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
       <RouterProvider router={router} />
     </AuthProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
